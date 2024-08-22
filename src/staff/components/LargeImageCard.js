c9bf98fa22e6b000351197387e6a95184323326d
Abid Talukder <atalukder30@stuy.edu>
2024-08-22 11:41:22 -0400

    Made this component dynamic

--- src/staff/components/LargeImageCard.js
+++ src/staff/components/LargeImageCard.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const LargeImageCard = ({ to, image, title }) => {
   return (
     <Link to={to} className="no-underline">
-      <div className="lg-img-card border-2 border-gray-500 card hover:shadow-md hover:border-black duration-175">
+      <div className="lg-img-card border-2 border-gray-400 card hover:shadow-md hover:border-black duration-175">
         <figure>
           <img src={image} alt={title} />
         </figure>
