a3d0c5c41fc967552f3278d30d5d0ddd04fef5b5
Abid Talukder <atalukder30@stuy.edu>
2024-09-06 07:24:55 -0400

    Added lazy loading and code splitting to reduce bundle sizes by 50%

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import logo from "./logo.svg";
-import { Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { Suspense } from "react";
+import { lazy } from "react";
 import "./App.css";
+
 import Home from "./shared/pages/Home";
-import PageNotFound from "./shared/pages/404";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
-import Jobs from "./opportunities/pages/Jobs";
-import Browse from "./staff/pages/Browse";
-import Profile from "./staff/pages/Profile";
-import Center from "./staff/pages/Center";
-import CreatePost from "./staff/pages/CreatePost";
+import StickyFooter from "./shared/components/Navigation/StickyFooter.js";
 import IndividualPost from "./opportunities/pages/IndividualPost";
-import ProfilePage from "./shared/pages/Profile.js";
 import SignIn from "./shared/pages/SignIn";
 import { GlobalContextProvider } from "./context/global/GlobalContextProvider.js";
-import useAuthActions from "./context/global/authActions.js";
-import StickyFooter from "./shared/components/Navigation/StickyFooter.js";
+
+const PageNotFound = lazy(() => import("./shared/pages/404.js"));
+const Jobs = lazy(() => import("./opportunities/pages/Jobs.js"));
+const Browse = lazy(() => import("./staff/pages/Browse"));
+const Profile = lazy(() => import("./staff/pages/Profile"));
+const Center = lazy(() => import("./staff/pages/Center"));
+const CreatePost = lazy(() => import("./staff/pages/CreatePost"));
+const ProfilePage = lazy(() => import("./shared/pages/Profile.js"));
 
 function App() {
   return (
@@ -22,29 +24,27 @@ function App() {
       <section>
         <MainNavigation />
         <main className=" container-xl ">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/jobs" element={<Jobs />} />
-            <Route path="/staff" element={<Browse />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/center/:centerName" element={<Center />} />
-            <Route path="/staff/:staffId" element={<Profile />} />
-            <Route path="/createPost" element={<CreatePost />} />
-            <Route
-              path="/editPost/:postID"
-              element={<CreatePost edit={true} />}
-            />
-            <Route path="/post/:postID" element={<IndividualPost />} />
-            <Route path="/signInTemporary" element={<SignIn />} />
-            <Route path="/signOut" element={<Home signOut={true} />} />
-            <Route path="/signIn" element={<Home signIn={true} />} />
+          <Suspense fallback={<div>Loading ...</div>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/jobs" element={<Jobs />} />
+              <Route path="/staff" element={<Browse />} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/center/:centerName" element={<Center />} />
+              <Route path="/staff/:staffId" element={<Profile />} />
+              <Route path="/createPost" element={<CreatePost />} />
+              <Route
+                path="/editPost/:postID"
+                element={<CreatePost edit={true} />}
+              />
+              <Route path="/post/:postID" element={<IndividualPost />} />
+              <Route path="/signInTemporary" element={<SignIn />} />
+              <Route path="/signOut" element={<Home signOut={true} />} />
+              <Route path="/signIn" element={<Home signIn={true} />} />
 
-            <Route path="/health">
-              <p>App is Healthy</p>
-            </Route>
-
-            <Route path="/*" element={<PageNotFound />} />
-          </Routes>
+              <Route path="/*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </main>
         <StickyFooter />
       </section>
